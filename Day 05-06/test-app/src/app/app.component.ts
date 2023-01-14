33b0e9bd24bd741683f607aa0d28b455d9921f44
Smit ITVedant <smitjoshi@itvedant.com>
2023-01-14 16:16:47 +0530

    initial commit

---
+++ Day 05-06/test-app/src/app/app.component.ts
@@ -0,0 +1,101 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  fontcolor = "pink";
+  id = 101;
+  name = "ABCD EFGH";
+  salary = 1234;
+// Creating an object of emp
+  emp = {
+    'id': '102',
+    'name': 'IJKL MNOP',
+    'salary': 235601
+  }
+  // Single Style Binding e.g.
+  singleStyle = "red";
+  // Multiple Style Binding e.g.
+  multipleStyle = "color:green; font-size:28px; text-decoration:underline";
+
+  // Single Class Binding e.g.
+  x = "x";
+  // Multiple Class Binding e.g.
+  pc1 = true;
+  pc2 = true;
+  pc3 = true;
+
+  multipleSelector = {
+    'c1': this.pc1,
+    'c2': this.pc2,
+    'c3': this.pc3
+  }
+  // Conditional Binding e.g.
+  condition = true;
+
+  /*
+    Two way data binding -
+      It refers to sharing data between component class and it's template
+      It is automatic synchronization of data. So if you change data in one place, it will automatically reflect at another place.
+      This is useful for input validation and manipulation.
+      Two Way Data Binding is defined with 'ngModel'. It's syntax is: [(ngModel)]
+      For the two way data binding, we need to import module named as FormsModule
+  */
+ myText = "";
+
+//  Event Binding
+func() {
+  alert("Event Binding Triggered!!!");
+  }
+
+  setColor = '';
+
+  redColor() {
+    this.setColor = "red";
+  }
+  blueColor() {
+    this.setColor = "yellow";
+  }
+/*                     Data Types
+      Data Types                      Uses
+      1. number                       It can store whole as well as decimal numbers.
+      2. boolean                      It represents true/false.
+      3. string                       It represents sequence of characters.
+      4. void                         It represents function return types.
+      5. null                         It is used when object does not have any value.
+      6. undefined                    It denotes un-initialized value.
+      7. any                          It is used to store any type of value.
+
+      Syntax for variable declaration in TypeScript
+      varname : datatype
+*/
+/* Template Reference Variable - 
+      Syntax: <input type="text" #reference_var>
+*/
+
+/*      Directives - 
+          They are defined as classes that can add new behaviour to the elements in the template or modify existing behaviour.
+          
+        Built-in attribute directives:
+        i.   NgClass
+        ii.  NgStyle
+        iii. NgModel
+
+        Built-in structural directives:
+          It is responsible for HTML Layout.
+          They shape or re-shape DOM structure by adding, removing and manipulating the host element.
+
+          i.   NgIf -
+                  Syntax: <element *ngIf="condition"></element>
+                  OR
+                  Syntax: <element *ngIf="condition then refVar1 else refVar2"></element>
+          ii.  NgSwitch
+          iii. NgFor
+*/
+
+// EOL
+}
---
+++ Day 05-06/test-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { Child2Component } from './child2/child2.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChildComponent,
+    Child2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
